d65df5956ce6eeb0565a8d46a8afccb54eeb7dc6
ak8006181716 <153820262+ak8006181716@users.noreply.github.com>
2025-09-08 23:39:03 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "*",
+    origin: ["http://localhost:5173", "https://your-frontend-domain.vercel.app"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
